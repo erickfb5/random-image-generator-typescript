89875b0d7e6587c2e0eeaec496600af9f0063c63
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-01-23 17:04:24 -0300

    fixing errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [imgs, setImgs] = useState<string[]>([]);
+
+  const generateRandomImg = (): void => {
+    const unsplashURL = "https://source.unsplash.com/random/";
+    const rows = 6;
+
+    const getRandSize = (): string => `${getRandNum()}x${getRandNum()}`;
+    const getRandNum = (): number => Math.floor(Math.random() * 10) + 300;
+
+    const newImgs = [];
+    for (let i = 0; i < rows; i++) {
+      newImgs.push(`${unsplashURL}${getRandSize()}`);
+    }
+
+    setImgs(newImgs);
+  };
+
+  useEffect(() => {
+    generateRandomImg();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="title">
+        <button onClick={generateRandomImg}>
+          <h1 className="title">Generate Images</h1>
+        </button>
+      </div>
+      <br />
+      <div className="container">
+        {imgs.map((img, index) => (
+          <img key={index} src={img} alt={`Random Image ${index}`} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
